dda57fc5ed19fca935b0908788f6cc064d590ec2
Tushar <tm2582001@gmail.com>
2023-02-07 12:23:27 +0530

    changed js to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 import CardList from "./component/card-list/card-list.component";
 import SearchBox from "./component/search-box/search-box.component";
+
+import { getData } from "./utils/data.utils";
 import "./App.css";
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
 
 const App = () => {
   const [searchField, setSearchField] = useState(""); // [value, setValue]
-  const [title, setTitle] = useState('');
-  const [monsters, setMonsters] = useState([]);
+  // const [title, setTitle] = useState('');
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters,setFilteredMonsters] = useState(monsters);
 
   console.log("render");
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((users) => setMonsters(users))
-      .catch((e) => console.log(e));
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((res) => res.json())
+    //   .then((users) => setMonsters(users))
+    //   .catch((e) => console.log(e));
+
+    const fetchUsers = async()=>{
+      const users = await getData<Monster[]>("https://jsonplaceholder.typicode.com/users");
+      setMonsters(users);
+    };
+
+    fetchUsers();
   }, []);
 
   useEffect(()=>{
@@ -25,29 +40,30 @@ const App = () => {
     setFilteredMonsters(newFilteredMonsters);
   },[monsters,searchField]);
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
-  const onTitleChange = (event) => {
-    const searchFieldString = event.target.value.toLowerCase();
-    setTitle(searchFieldString);
-  };
+  // const onTitleChange = (event) => {
+  //   const searchFieldString = event.target.value.toLowerCase();
+  //   // setTitle(searchFieldString);
+  // };
 
   return (
     <div className="App">
-      <h1 className="app-title">{title}</h1>
+      <h1 className="app-title">Monster Rolodex</h1>
+      {/* <h1 className="app-title">{title}</h1> */}
       <SearchBox
         onChangeHandler={onSearchChange}
         className="monsters-search-box"
         placeholder="search monsters"
       />
       <br />
-      <SearchBox
+      {/* <SearchBox
         onChangeHandler={onTitleChange}
         className="monsters-search-box"
         placeholder="search monsters"
-      />
+      /> */}
       <CardList monsters={filteredMonsters} />
     </div>
   );
